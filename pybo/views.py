d6af03a640a23f285fdad4c1d4e43b62fc73de80
dongpakka <kimvjgd@naver.com>
2022-01-28 11:02:55 +0900

    migrate complete

--- pybo/views.py
+++ pybo/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.")
