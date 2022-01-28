63de5b98224205387e4bffc5ce6ec2a294502722
dongpakka <kimvjgd@naver.com>
2022-01-28 17:46:25 +0900

    today

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, detail
+from . import views
 
 app_name = 'pybo'
 
 urlpatterns = [
-    path('', index, name='index'),     # config/urls.py에서 'pybo/' + '' --> 'pybo/'            # 보통 첫페이지를 index 페이지라고 부른다.
-    path('<int:question_id>/', detail, name='detail'),
+    path('', views.index, name='index'),     # config/urls.py에서 'pybo/' + '' --> 'pybo/'            # 보통 첫페이지를 index 페이지라고 부른다.
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create ,name='answer_create'),
+
+    
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 
-from .models import Question
+from .models import Question, Answer
+from django.utils import timezone
 
 
 # Create your views here.
@@ -9,7 +10,11 @@ def index(request):
     pybo 목록 출력
     """
     question_list = Question.objects.order_by('-create_date')       # - 내림차순
-    context = {'question_list': question_list}          # key 이름 = context 변수
+    total_count = Question.objects.count()
+    context = {
+        'question_list': question_list,
+        'total_count':total_count
+    }          # key 이름 = context 변수
     
     return render(request, 'pybo/question_list.html', context)
     # return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.")
@@ -22,3 +27,23 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    # content = request.POST['content'] # content키가 없으면 - 예외가 발생
+    content = request.POST.get('content')    # content가 없어도 none리턴
+
+    # Answer 생성
+      # 방법 1
+    # answer = Answer(question = question, 
+    #                 content = content,
+    #                 create_date = timezone.now())
+    # answer.save()
+      # 방법 2] ForeignKey 관계인 경우
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail',question_id=question_id)
+    # return redirect('pybo:detail',question_id=question_id)
+
