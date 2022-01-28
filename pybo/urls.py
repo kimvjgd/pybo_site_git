876198ff4a1d088feddb09a60c589d77e550076d
dongpakka <kimvjgd@naver.com>
2022-01-28 13:22:18 +0900

    pybo 목록/상세보기

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, detail
+
+app_name = 'pybo'
 
 urlpatterns = [
-    path('', index),     # config/urls.py에서 'pybo/' + '' --> 'pybo/'
+    path('', index, name='index'),     # config/urls.py에서 'pybo/' + '' --> 'pybo/'            # 보통 첫페이지를 index 페이지라고 부른다.
+    path('<int:question_id>/', detail, name='detail'),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.")
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')       # - 내림차순
+    context = {'question_list': question_list}          # key 이름 = context 변수
+    
+    return render(request, 'pybo/question_list.html', context)
+    # return HttpResponse("안녕하세요 pybo에 오신 것을 환영합니다.")
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
