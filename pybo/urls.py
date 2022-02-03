07462656be4807b62dd5782a6c3173d4093905dc
dongpakka <kimvjgd@naver.com>
2022-02-03 13:25:46 +0900

    2장 완성

--- pybo/urls.py
+++ pybo/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),     # config/urls.py에서 'pybo/' + '' --> 'pybo/'            # 보통 첫페이지를 index 페이지라고 부른다.
     path('<int:question_id>/', views.detail, name='detail'),
     path('answer/create/<int:question_id>/', views.answer_create ,name='answer_create'),
-
+    path('question/create/', views.question_create, name='question_create'),
     
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 
 from .models import Question, Answer
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 
 
 # Create your views here.
@@ -32,18 +33,41 @@ def answer_create(request, question_id):
     """
     pybo 답변등록
     """
-    question = get_object_or_404(Question, pk=question_id)
-    # content = request.POST['content'] # content키가 없으면 - 예외가 발생
-    content = request.POST.get('content')    # content가 없어도 none리턴
+        # 방법 1
+    question = get_object_or_404(Question, pk=question_id) 
+    if request.method == "POST":
+      form = AnswerForm(request.POST) 
+      if form.is_valid():
+        answer = form.save(commit=False)
+        answer.create_date = timezone.now()
+        answer.question = question      # FK 처리
+        answer.save()
+        return redirect('pybo:detail', question_id=question.id)
+    else:
+      form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'pybo/question_detail.html', context)
 
-    # Answer 생성
-      # 방법 1
-    # answer = Answer(question = question, 
-    #                 content = content,
-    #                 create_date = timezone.now())
-    # answer.save()
-      # 방법 2] ForeignKey 관계인 경우
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('pybo:detail',question_id=question_id)
-    # return redirect('pybo:detail',question_id=question_id)
+
+        # 방법 2] ForeignKey 관계인 경우
+      # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+      # return redirect('pybo:detail',question_id=question_id)
+      # return redirect('pybo:detail',question_id=question_id)
+
+
+def question_create(request): 
+  """
+  pybo 질문등록
+  """
+  if request.method == 'POST':
+    form = QuestionForm(request.POST)
+    if form.is_valid():    # 유효성 검사
+      question = form.save(commit=False)    # form 정보를 model에 저장하라
+      question.create_date = timezone.now()
+      question.save()
+      return redirect('pybo:index')
+  else:
+    form = QuestionForm()
+  context = {'form':form}
+  return render(request, 'pybo/question_form.html', context)
 
